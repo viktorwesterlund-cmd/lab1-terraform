67bdb6f76554b2ee0191ab5ff9162f9d97994df4
Viktor <viktor.westerlund@chasacademy.se>
2026-03-14 17:04:55 +0100

    Add Terraform CI pipeline

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install -y ufw fail2ban unattended-upgrades
+
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw --force enable
+
+dpkg-reconfigure -plow unattended-upgrades
+
+echo "Startup script completed at $(date)" > /var/log/startup-complete.log
